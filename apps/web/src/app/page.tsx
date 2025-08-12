2f0f7ab06c69ca861358e949a95b359c4da0045b
daddia <jonathan@daddia.com>
2025-08-12 21:32:29 +1000

    Refactor typography styles in globals.css and clean up unused paragraph in Home component for improved readability.

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -5,8 +5,7 @@ export default function Home() {
         <h1 className="text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl dark:text-white">
           daddia
         </h1>
-        <p className="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8 dark:text-gray-400">
-        </p>
+        <p className="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8 dark:text-gray-400"></p>
       </div>
     </div>
   );
